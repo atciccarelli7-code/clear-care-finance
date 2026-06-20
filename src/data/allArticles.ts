4987bfff7e8dab30de19387ed9a803688822dcf6
atciccarelli7-code <atciccarelli7@gmail.com>
2026-06-20 15:47:52 -0400

    Exclude legacy placeholders from article collection

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -1,5 +1,6 @@
 import { SITE_ARTICLES } from "./siteArticles";
 import { OBBB_OVERTIME_ARTICLE } from "./healthcareWorkerArticles";
 import { BACKUP_CARE_ARTICLE } from "./familyLogisticsArticles";
+import { publishedArticles } from "@/lib/article-status";
 
-export const ALL_ARTICLES = [OBBB_OVERTIME_ARTICLE, BACKUP_CARE_ARTICLE, ...SITE_ARTICLES];
+export const ALL_ARTICLES = publishedArticles([OBBB_OVERTIME_ARTICLE, BACKUP_CARE_ARTICLE, ...SITE_ARTICLES]);

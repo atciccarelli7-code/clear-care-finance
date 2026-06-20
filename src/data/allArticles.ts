ae292be97284d6143b02e12efe0e4aedc3241780
atciccarelli7-code <atciccarelli7@gmail.com>
2026-06-20 15:46:26 -0400

    Include backup care article in article collection

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -1,4 +1,5 @@
 import { SITE_ARTICLES } from "./siteArticles";
 import { OBBB_OVERTIME_ARTICLE } from "./healthcareWorkerArticles";
+import { BACKUP_CARE_ARTICLE } from "./familyLogisticsArticles";
 
-export const ALL_ARTICLES = [OBBB_OVERTIME_ARTICLE, ...SITE_ARTICLES];
+export const ALL_ARTICLES = [OBBB_OVERTIME_ARTICLE, BACKUP_CARE_ARTICLE, ...SITE_ARTICLES];

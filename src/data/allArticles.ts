d7c828d493f7b602a78d90ce9f8045abcc229aa5
atciccarelli7-code <atciccarelli7@gmail.com>
2026-06-20 16:18:06 -0400

    Include January 2027 Medicare Medicaid article

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -1,6 +1,12 @@
 import { SITE_ARTICLES } from "./siteArticles";
 import { OBBB_OVERTIME_ARTICLE } from "./healthcareWorkerArticles";
 import { BACKUP_CARE_ARTICLE } from "./familyLogisticsArticles";
+import { JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE } from "./policyChangeArticles";
 import { publishedArticles } from "@/lib/article-status";
 
-export const ALL_ARTICLES = publishedArticles([OBBB_OVERTIME_ARTICLE, BACKUP_CARE_ARTICLE, ...SITE_ARTICLES]);
+export const ALL_ARTICLES = publishedArticles([
+  JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE,
+  OBBB_OVERTIME_ARTICLE,
+  BACKUP_CARE_ARTICLE,
+  ...SITE_ARTICLES,
+]);

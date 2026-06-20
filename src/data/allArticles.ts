3e9d6bf487539fb4010cd2b1b027cd7b3d62b754
atciccarelli7-code <atciccarelli7@gmail.com>
2026-06-19 21:34:38 -0400

    Expose healthcare worker article in article collections

---
+++ src/data/allArticles.ts
@@ -0,0 +1,4 @@
+import { SITE_ARTICLES } from "./siteArticles";
+import { OBBB_OVERTIME_ARTICLE } from "./healthcareWorkerArticles";
+
+export const ALL_ARTICLES = [OBBB_OVERTIME_ARTICLE, ...SITE_ARTICLES];

e99b909e8f7b4ef9f8a3d1b732de057698203611
atciccarelli7-code <atciccarelli7@gmail.com>
2026-06-21 08:27:36 -0400

    Register healthcare confusion article pack

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -3,9 +3,11 @@ import { OBBB_OVERTIME_ARTICLE } from "./healthcareWorkerArticles";
 import { BACKUP_CARE_ARTICLE } from "./familyLogisticsArticles";
 import { JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE } from "./policyChangeArticles";
 import { HSA_FSA_ARTICLE } from "./hsaFsaArticle";
+import { HEALTHCARE_CONFUSION_ARTICLES } from "./healthcareConfusionArticles";
 import { publishedArticles } from "@/lib/article-status";
 
 export const ALL_ARTICLES = publishedArticles([
+  ...HEALTHCARE_CONFUSION_ARTICLES,
   HSA_FSA_ARTICLE,
   JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE,
   OBBB_OVERTIME_ARTICLE,

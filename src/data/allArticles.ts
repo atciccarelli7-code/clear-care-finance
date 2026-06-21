2097ee23d90c1cbbbe72323e4244939a4ea3a7a3
atciccarelli7-code <atciccarelli7@gmail.com>
2026-06-21 08:51:42 -0400

    Register open enrollment article pack

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -4,9 +4,11 @@ import { BACKUP_CARE_ARTICLE } from "./familyLogisticsArticles";
 import { JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE } from "./policyChangeArticles";
 import { HSA_FSA_ARTICLE } from "./hsaFsaArticle";
 import { HEALTHCARE_CONFUSION_ARTICLES } from "./healthcareConfusionArticles";
+import { OPEN_ENROLLMENT_ARTICLES } from "./openEnrollmentArticles";
 import { publishedArticles } from "@/lib/article-status";
 
 export const ALL_ARTICLES = publishedArticles([
+  ...OPEN_ENROLLMENT_ARTICLES,
   ...HEALTHCARE_CONFUSION_ARTICLES,
   HSA_FSA_ARTICLE,
   JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE,

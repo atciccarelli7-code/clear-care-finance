ab2cbe672fddb8e97eb585361df0068ad9bf06a3
atciccarelli7-code <atciccarelli7@gmail.com>
2026-06-24 10:51:41 -0400

    feat: include prepared post-AdSense articles

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -5,6 +5,7 @@ import { JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE } from "./policyChangeAr
 import { HSA_FSA_ARTICLE } from "./hsaFsaArticle";
 import { HEALTHCARE_CONFUSION_ARTICLES } from "./healthcareConfusionArticles";
 import { OPEN_ENROLLMENT_ARTICLES } from "./openEnrollmentArticles";
+import { POST_ADSENSE_PREPARED_ARTICLES } from "./postAdsensePreparedArticles";
 import { SOURCE_PRESETS } from "./sources";
 import { publishedArticles } from "@/lib/article-status";
 
@@ -20,6 +21,7 @@ const OPEN_ENROLLMENT_ARTICLES_READY = OPEN_ENROLLMENT_ARTICLES.map((article) =>
 export const ALL_ARTICLES = publishedArticles([
   ...OPEN_ENROLLMENT_ARTICLES_READY,
   ...HEALTHCARE_CONFUSION_ARTICLES,
+  ...POST_ADSENSE_PREPARED_ARTICLES,
   HSA_FSA_ARTICLE,
   JANUARY_2027_MEDICARE_MEDICAID_CHANGES_ARTICLE,
   OBBB_OVERTIME_ARTICLE,

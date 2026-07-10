096a7e31078f6a49771e99d713bbfc1b3b44905f
atciccarelli7-code <atciccarelli7@gmail.com>
2026-07-10 15:09:27 -0400

    Install route-aware AdSense loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { createRoot, hydrateRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { installRouteAwareAdSense } from "./lib/routeAwareAdSense";
 
 const container = document.getElementById("root");
 
 if (!container) {
   throw new Error("Application root element was not found.");
 }
+
+installRouteAwareAdSense();
 
 if (container.hasChildNodes()) {
   hydrateRoot(container, <App />);

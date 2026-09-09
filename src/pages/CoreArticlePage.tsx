c2a87a571a183c5cde4583424da17c2004086b3b
atciccarelli7-code <atciccarelli7@gmail.com>
2026-09-08 20:17:08 -0400

    Resolve article routes from unified catalog

--- src/pages/CoreArticlePage.tsx
+++ src/pages/CoreArticlePage.tsx
@@ -1,14 +1,14 @@
 import { Navigate, useParams } from "react-router-dom";
-import { CORE_ARTICLES } from "@/data/coreArticles";
+import { ALL_ARTICLES } from "@/data/allArticles";
 import { ArticlePageView } from "./ArticlePage";
 
 const CoreArticlePage = () => {
   const { slug = "" } = useParams();
-  const article = CORE_ARTICLES.find((candidate) => candidate.slug === slug);
+  const article = ALL_ARTICLES.find((candidate) => candidate.slug === slug);
 
   if (!article) return <Navigate to="/articles" replace />;
 
-  return <ArticlePageView article={article} articleCatalog={CORE_ARTICLES} />;
+  return <ArticlePageView article={article} articleCatalog={ALL_ARTICLES} />;
 };
 
 export default CoreArticlePage;

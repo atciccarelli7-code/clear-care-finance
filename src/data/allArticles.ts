8e5adbb1c2ad58e9ec9cd7db58cdc7a159096717
atciccarelli7-code <atciccarelli7@gmail.com>
2026-09-08 21:17:04 -0400

    Load full review metadata only with article catalog

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -2,7 +2,7 @@ import { CORE_ARTICLES } from "./coreArticles";
 import { FOUNDER_CARE_TRANSITIONS_ARTICLES } from "./founderCareTransitionsArticles";
 import { FOUNDER_HOSPITAL_ECONOMICS_ARTICLES } from "./founderHospitalEconomicsArticles";
 import { FOUNDER_HEALTHCARE_SYSTEMS_ARTICLES } from "./founderHealthcareSystemsArticles";
-import { applyPublisherArticleReviewMetadata } from "./publisherArticleReviews";
+import { applyPublisherArticleReviewMetadata } from "./publisherArticleReviewMetadata";
 import { publishedArticles } from "@/lib/article-status";
 
 const BASE_ARTICLES = [

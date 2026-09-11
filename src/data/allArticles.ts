17dd38af3cddd07d31d7da0a7757237994b8690e
atciccarelli7-code <atciccarelli7@gmail.com>
2026-09-11 03:34:12 -0400

    Register hospital money founder article

--- src/data/allArticles.ts
+++ src/data/allArticles.ts
@@ -1,12 +1,14 @@
 import { CORE_ARTICLES } from "./coreArticles";
 import { FOUNDER_CARE_TRANSITIONS_ARTICLES } from "./founderCareTransitionsArticles";
 import { FOUNDER_HOSPITAL_ECONOMICS_ARTICLES } from "./founderHospitalEconomicsArticles";
+import { FOUNDER_HOSPITAL_MONEY_ARTICLES } from "./founderHospitalMoneyArticles";
 import { FOUNDER_HEALTHCARE_SYSTEMS_ARTICLES } from "./founderHealthcareSystemsArticles";
 import { applyPublisherArticleReviewMetadata } from "./publisherArticleReviewMetadata";
 import { publishedArticles } from "@/lib/article-status";
 
 const BASE_ARTICLES = [
   ...FOUNDER_CARE_TRANSITIONS_ARTICLES,
+  ...FOUNDER_HOSPITAL_MONEY_ARTICLES,
   ...FOUNDER_HOSPITAL_ECONOMICS_ARTICLES,
   ...FOUNDER_HEALTHCARE_SYSTEMS_ARTICLES,
   ...CORE_ARTICLES,
